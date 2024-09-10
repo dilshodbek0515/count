eb3991ad654d35011ef996fd6382f2e3dbc200b4
dilshod <dilshodx516@gmail.com>
2024-09-11 04:23:27 +0500

    count

---
+++ app.js
@@ -0,0 +1,34 @@
+const heading = document.querySelector(".text")
+const audio = document.querySelector(".audio")
+console.log(audio);
+
+let off = 0
+
+function inc() {
+    if (off < 10) {
+        off++
+    }
+    else if (off < 100) {
+        off += 10
+        audio.play()
+    } else if (off < 1000) {
+        off += 100
+    }
+    else {
+        return null
+    }
+
+    heading.innerHTML = off
+}
+
+function dec() {
+    if (off > 100) {
+        off -= 100
+    } else if (off > 10) {
+        off -= 10
+    } else if (off > 0) {
+        off--
+    }
+
+    heading.innerHTML = off
+}
